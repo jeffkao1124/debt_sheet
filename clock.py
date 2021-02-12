1343d59a55b2cce856524ad5fdf6167b22e341d2
tzucheng <f84066024@gs.ncku.edu.tw>
2021-02-12 16:03:59 +0800

    clock_up

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 sched = BlockingScheduler()
 
 # 定義排程 : 在周一至周五，每 20 分鐘就做一次 def scheduled_jog()
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
+@sched.scheduled_job('cron', day_of_week='mon-sun', minute='*/20')
 def scheduled_job():
     url = "https://debt0sheet.herokuapp.com/"
     connect = urlopen(url)
